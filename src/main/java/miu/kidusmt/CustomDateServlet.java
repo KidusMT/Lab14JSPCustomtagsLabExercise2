c196d01580e84da09405e7fe2877b0f86fe6ab1b
KidusMT <kidusmamuye@gmail.com>
2021-05-09 16:11:24 -0500

    first commit

---
+++ src/main/java/miu/kidusmt/CustomDateServlet.java
@@ -0,0 +1,19 @@
+package miu.kidusmt;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author KidusMT
+ * Date: 5/9/2021
+ */
+public class CustomDateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
