262cbc7a79be5fd496f249fbc18c606bd66b4e60
KidusMT <kidusmamuye@gmail.com>
2021-05-09 16:49:12 -0500

    done with the custom JSTL implementation

--- src/main/java/miu/kidusmt/CustomDateServlet.java
+++ src/main/java/miu/kidusmt/CustomDateServlet.java
@@ -1,6 +1,7 @@
 package miu.kidusmt;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,10 +11,11 @@ import java.io.IOException;
  * @author KidusMT
  * Date: 5/9/2021
  */
+@WebServlet({"/"})
 public class CustomDateServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        req.getRequestDispatcher("showCurrentDate.jsp").forward(req, resp);
     }
 }
